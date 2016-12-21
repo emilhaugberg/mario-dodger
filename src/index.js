7adde6e7f0d00b38e61eac1e412d0225a38959d2
Emil Haugberg <emilveen@gmail.com>
2016-12-21 21:20:29 +0100

    Make balls move.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 var $ = require('jquery')
 var R = require('ramda')
 
+var frames  = 0
+var seconds = 0
+
 var dimensions = {
-  width: 600,
+  width: 1000,
   height: 600
 }
 
@@ -18,10 +21,12 @@ var state = {
     x: dimensions.width / 2,
     y: dimensions.height / 2,
     r: 20
-  }
+  },
+  balls: []
 }
 
 var SPEED = 5
+var BALLSPEED = 3
 
 var keyDownHandler = (e) => {
 
@@ -54,33 +59,91 @@ var keyUpHandler = (e) => {
   }
 }
 
-var drawPlayer = (ctx) => {
-  ctx.beginPath();
-  ctx.arc(state.player.x, state.player.y, state.player.r, 0, Math.PI * 2, false);
-  ctx.fillStyle = "white";
-  ctx.fill();
-  ctx.closePath();
+var drawPlayer = () => {
+  ctx.beginPath()
+  ctx.arc(state.player.x, state.player.y, state.player.r, 0, Math.PI * 2, false)
+  ctx.fillStyle = "red"
+  ctx.fill()
+  ctx.closePath()
 }
 
-var draw = (canvas, ctx) => {
-  ctx.clearRect(0, 0, canvas.width, canvas.height)
-  drawPlayer(ctx)
-
+var movePlayer = () => {
   if (state.keypresses.left) {
-    state.player.x -= SPEED
+    if (state.player.x > state.player.r) {
+      state.player.x -= SPEED
+    }
   }
 
   if (state.keypresses.right) {
-    state.player.x += SPEED
+    if (state.player.x < (dimensions.width - state.player.r)) {
+      state.player.x += SPEED
+    }
   }
 
   if (state.keypresses.up) {
-    state.player.y -= SPEED
+    if (state.player.y > state.player.r) {
+      state.player.y -= SPEED
+    }
   }
 
   if (state.keypresses.down) {
-    state.player.y += SPEED
+    if (state.player.y < (dimensions.height - state.player.r)) {
+      state.player.y += SPEED
+    }
   }
+}
+
+var random = (min, max) => Math.random() * (max - min) + min
+
+var addBall = () => {
+  var randomR = Math.floor(random(10, 20))
+  var ball = {
+    x: Math.floor(random(randomR, dimensions.width - randomR)),
+    y: 0,
+    r: randomR
+  }
+
+  state.balls = R.append(ball, state.balls)
+}
+
+var drawBall = () => {
+  R.forEach((ball) => {
+    ctx.beginPath()
+    ctx.arc(ball.x, ball.y, ball.r, 0, Math.PI * 2, false)
+    ctx.fillStyle = "blue"
+    ctx.fill()
+    ctx.closePath()
+  }, state.balls)
+}
+
+var moveBalls = () => {
+  state.balls = R.map((ball) => {
+    return { x: ball.x, y: ball.y += BALLSPEED, r: ball.r }
+  }, state.balls)
+}
+
+var removeObsoleteBalls = () => {
+  state.balls = R.reject((ball) => {
+    return (ball.y > dimensions.height - ball.r)
+  }, state.balls)
+}
+
+var draw = () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+  drawPlayer()
+  movePlayer()
+
+  frames += 1
+  if (frames % 100 == 0) seconds += 1
+  if (frames % 20 == 0) addBall()
+
+  moveBalls()
+  drawBall()
+  removeObsoleteBalls()
+
+  console.log(R.length(state.balls))
+
+  $('#seconds').html(seconds)
 }
 
 var canvas = $('#game')[0]
@@ -89,4 +152,4 @@ var ctx = canvas.getContext('2d')
 $(document).keydown(keyDownHandler)
 $(document).keyup(keyUpHandler)
 
-setInterval(() => draw(canvas, ctx), 10)
+setInterval(() => draw(), 10)
