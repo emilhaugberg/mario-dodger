e8ee317636a038f92c8a97c3311e83a98cf11b2a
Emil Haugberg <emilveen@gmail.com>
2016-12-21 14:45:53 +0100

    Setup canvas.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,31 @@
-var $ = require('jquery');
-var R = require('ramda');
+var $ = require('jquery')
+var R = require('ramda')
+
+var CANVASWIDTH = 600
+var CANVASHEIGHT = 600
+
+var PLAYERRADIUS = 20
+
+var initialPosition = {
+  x: CANVASWIDTH / 2,
+  y: CANVASHEIGHT / 2,
+  r: PLAYERRADIUS
+}
+
+var canvas = $('#game')[0]
+var ctx = canvas.getContext('2d')
+
+function drawPlayer (ctx) {
+  ctx.beginPath();
+  ctx.arc(initialPosition.x, initialPosition.y, initialPosition.r, 0, Math.PI * 2, false);
+  ctx.fillStyle = "white";
+  ctx.fill();
+  ctx.closePath();
+}
+
+function draw (canvas, ctx) {
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+  drawPlayer (ctx)
+}
+
+setInterval(draw(canvas, ctx), 10)
